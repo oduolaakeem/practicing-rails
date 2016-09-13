9ef0c8bb94d79f86a9edf0a63ee3b4e0ef589903
Chen ShangYo <shangyo.chen@gmail.com>
2016-09-13 10:48:39 +0000

    Finish layout and routes

--- twittor/test/controllers/static_pages_controller_test.rb
+++ twittor/test/controllers/static_pages_controller_test.rb
@@ -8,15 +8,27 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | Twittor"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Twittor"
+  end
+  
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | Twittor"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Twittor"
   end
 
 end
