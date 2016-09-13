2749085ffaa9f0da917092c7dcc972cd4487770f
csyo <shangyo.chen@gmail.com>
2016-09-13 23:03:47 +0800

    User signup

---
+++ twittor/test/integration/user_signup_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference("User.count") do
+      post users_path, params: { user: {
+        name: '',
+        email: '',
+        password: ''
+        }}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference("User.count", 1) do
+      post users_path, params: { user: {
+        name: 'User',
+        email: 'user@example.com',
+        password: 'password'
+        }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    # No errors shown
+    divs = css_select 'div#error_explanation'
+    assert_equal divs.length, 0
+    # Flash messag exists
+    assert_not flash.empty?
+  end
+end
