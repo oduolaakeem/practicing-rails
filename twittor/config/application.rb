f018f3d34602476388cea9aa971e6a819952af20
Chen ShangYo <shangyo.chen@gmail.com>
2016-09-13 08:39:54 +0000

    Finish static pages

--- twittor/config/application.rb
+++ twittor/config/application.rb
@@ -21,5 +21,9 @@ module Twittor
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
