ad5030988ae72412d7a0fbe176b7c2586bd8059f
csyo <shangyo.chen@gmail.com>
2016-10-01 01:50:56 +0800

    Empty Cart

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @cart = carts(:one)
+    # Add item in order to create cart
+    post line_items_url, params: { product_id: products(:ruby).id }
+    @cart = Cart.find(session[:cart_id])
   end
 
   test "should show cart" do
@@ -15,6 +17,6 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
       delete cart_url(@cart)
     end
 
-    assert_redirected_to carts_url
+    assert_redirected_to store_index_url
   end
 end
