2764ff3b4f1b57e32c32dc2149b88a0443430780
csyo <shangyo.chen@gmail.com>
2016-10-02 15:54:24 +0800

    Check Out & Place Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,20 @@
 class Order < ApplicationRecord
+  # OPTIMIZE: Use symbol instead of string as keys
   enum pay_type: {
     'Check' =>          0,
     'Credit Card' =>    1,
     'Purchase Order' => 2
   }
 
+  has_many :line_items, dependent: :destroy
+
   validates :name, :email, :address, presence: true
   validates :pay_type, inclusion: pay_types.keys
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
